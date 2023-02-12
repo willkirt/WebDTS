0c09fb1c509d8ed435335b065341fe2c3e02e8d7
William Kirtlink <wmkirtlink@email.neit.edu>
2023-02-12 16:30:50 -0500

    sd

--- main.js
+++ main.js
@@ -26,25 +26,6 @@ mongoose.connect(db.mongoURI,{
 require("./config/defectTrackerSchema.js");
 var DefectList = mongoose.model("defectList");
 
-// Main route
-app.get("/", function(req, res){
-    res.redirect('newBug.html');
-});
-
-app.get("/getDefectList", function(req,res){
-    DefectList.find({}).then(function(index){
-        res.json({index});
-    });
-});
-
-app.get("/addBugPage", function(req,res){
-    console.log("add a new bug?");
-})
-
-app.get("/deleteDefect", function(req,res){
-    console.log("Delete requested.");
-})
-
 app.listen(port, function(){
     console.log(`Running on port ${port}.`);
 })
